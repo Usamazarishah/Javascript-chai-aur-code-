3d50ca0aa015b65829c3bc29bbb29aeb26a77528
MUhammad Usama <usamazarishah@gmail.com>
2025-09-12 12:50:49 +0500

    done

---
+++ Basics/01_basics.js
@@ -0,0 +1,6 @@
+const accountID = 123456
+let accountEmail = "usama@google.com"
+var accountPassword = "112233"
+accountCity = "Karachi"
+
+console.table([accountID, accountEmail, accountPassword, accountCity])
