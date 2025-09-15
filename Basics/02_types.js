93997f3faeb8b02638c9372d33b365b79aa3b04a
MUhammad Usama <usamazarishah@gmail.com>
2025-09-15 12:09:01 +0500

    types done

---
+++ Basics/02_types.js
@@ -0,0 +1,39 @@
+
+// Primitive data types
+
+// let age = 21
+// let name = 
+
+// console.log(typeof age);
+// console.log( name);
+// console.log(typeof undefined);
+// console.log(typeof null);
+
+
+//Conversion Operations
+
+let score = 33
+let stringScore = ""
+
+console.log(score);
+console.log(typeof score);
+
+console.log(typeof (stringScore));
+
+const valueNumber = Number(stringScore);
+console.log(valueNumber);
+console.log(typeof valueNumber);
+
+// '33' => 33 (string to number)
+// '33abc' => NaN (Not a Number)
+// true => 1 ; false => 0
+
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+console.log(typeof booleanIsLoggedIn);
+
+// 1 => true ; 0 => false
+// "" => false
+// "abc" true
